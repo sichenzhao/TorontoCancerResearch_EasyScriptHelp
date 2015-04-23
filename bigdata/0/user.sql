da883c4f7e9b0b5a4d94017a5e428863970278cf
sichenzhao <szhao@SIchenZhaos-MacBook-Air.local>
2015-04-22 23:50:04 -0400

    should work

---
+++ bigdata/0/user.sql
@@ -0,0 +1,13 @@
+.open tmp.db
+.mode csv
+.separator "\t"
+.import clinical.tsv clinical
+.import ssm_open.tsv ssm_open
+CREATE TABLE distinct_ssm AS SELECT DISTINCT icgc_donor_id, project_code, chromosome, chromosome_start, chromosome_end, chromosome_strand, assembly_version, reference_genome_allele, mutated_from_allele, mutated_to_allele FROM ssm_open;
+CREATE TABLE fin_clinical AS SELECT icgc_donor_id, project_code, donor_age_at_diagnosis, donor_age_at_enrollment, donor_tumour_stage_at_diagnosis, tumour_grade FROM (SELECT DISTINCT icgc_donor_id, project_code, donor_age_at_diagnosis, donor_age_at_enrollment, donor_tumour_stage_at_diagnosis, tumour_grade FROM clinical) GROUP BY icgc_donor_id, project_code, donor_age_at_diagnosis, donor_age_at_enrollment, donor_tumour_stage_at_diagnosis, tumour_grade HAVING tumour_grade=max(tumour_grade);
+CREATE TABLE big_comb AS SELECT * FROM distinct_ssm LEFT OUTER JOIN fin_clinical USING (icgc_donor_id, project_code);
+CREATE TABLE final_result AS SELECT chromosome, chromosome_start, chromosome_end, chromosome_strand, assembly_version, reference_genome_allele, mutated_from_allele, mutated_to_allele, count() AS patient_number FROM big_comb GROUP BY chromosome, chromosome_start, chromosome_end, chromosome_strand, assembly_version, reference_genome_allele, mutated_from_allele, mutated_to_allele ORDER BY patient_number DESC;
+
+ALTER TABLE final_result ADD COLUMN tumour_grade TEXT;
+
+ALTER TABLE final_result ADD COLUMN donor_tumour_stage_at_diagnosis TEXT;
